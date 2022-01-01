217e152179db0454ff4253d0cdfc5747d02a9471
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-01-01 14:34:32 +0000

    remove:(chores): cleaning up the database and create a new schema

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
 import express from 'express';
+import postsRoute from './routes/posts';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/posts', postsRoute);
+
+app.listen(5000, () => {
+  console.log('listening to port:5000');
+});
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,20 +1,30 @@
 import { Router, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+
 import { ParsedUrlQuery } from 'querystring';
+
+export interface IParams extends ParsedUrlQuery {
+  userId: string;
+}
+
+export interface IPostData {
+  title: string;
+  userId: number;
+  content: string;
+}
+export interface TypedRequestBody<T> extends Request {
+  body: T;
+}
 
 const router = Router();
 const { post, user } = new PrismaClient();
-
-interface IParams extends ParsedUrlQuery {
-  userId: string;
-}
 
 router.get('/:user_id', async (req: Request, res: Response) => {
   const { userId } = req.params as IParams;
 
   const posts = await post.findMany({
     where: {
-      user_id: parseInt(userId),
+      userId: parseInt(userId),
     },
     select: {
       title: true,
@@ -24,5 +34,33 @@ router.get('/:user_id', async (req: Request, res: Response) => {
     },
   });
 
-  res.send(posts);
+  return res.send(posts);
 });
+
+router.post('/', async (req: TypedRequestBody<IPostData>, res: Response) => {
+  const { content, title, userId } = req.body;
+
+  const userExist = await user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!userExist) {
+    return res.status(400).json({
+      message: 'user not found',
+    });
+  }
+
+  const newPost = await post.create({
+    data: {
+      title,
+      userId,
+      post: content,
+    },
+  });
+
+  return res.json(newPost);
+});
+
+export default router;
